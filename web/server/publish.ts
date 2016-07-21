f562691215ad7be216a808b5377ef3c74731b2ee
Yannic Kilcher <yannic.kilcher@gmail.com>
2016-07-21 09:10:27 +0200

    authguard fix

--- web/server/publish.ts
+++ web/server/publish.ts
@@ -2,6 +2,11 @@ import {Meteor} from 'meteor/meteor';
 import {ShopItems} from '../imports/collections/collections';
 import * as _u from 'lodash';
 
+Meteor.publish('userData', function(){
+    return Meteor.users.find(this.userId);
+});
+
+
 Meteor.publish('shopitems', function(){
     return ShopItems.find();
 });
